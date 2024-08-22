6a623750cd03bde60b4e5a715ffa7cb2644ec0f4
acmors <marcosantoniosilva047@gmail.com>
2024-08-21 21:53:25 -0300

    implemetacao da logicas das operacoes

--- rest-api/src/main/java/com/example/restApi/MathController.java
+++ rest-api/src/main/java/com/example/restApi/MathController.java
@@ -23,27 +23,54 @@ public class MathController {
 	}
 	
 	@RequestMapping(value = "/minus/{numberOne}/{numberTwo}", method = RequestMethod.GET)
-	public Double minus(@PathVariable(value = "numberOne") String numberOne, @PathVariable(value = numberTwo) String numberTwo) {
+	public Double minus(@PathVariable(value = "numberOne") String numberOne, @PathVariable(value = "numberTwo") String numberTwo) {
 		if (!IsNumeric(numberOne) || !IsNumeric(numberTwo)){
 			throw new UnsupportedMathOperationException("set a numeric value");
 		}
 		return convertToDouble(numberOne) - convertToDouble(numberTwo);
 	}
 	
-	public Double multiplication() {
-		
+	@RequestMapping(value = "/multiplication/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+	public Double multiplication(@PathVariable(value = "numberOne") String numberOne, @PathVariable(value = "numberTwo") String numberTwo) {
+		if (!IsNumeric(numberOne) || !IsNumeric(numberTwo)){
+			throw new UnsupportedMathOperationException("set a numeric value");
+		}
+		return convertToDouble(numberOne) * convertToDouble(numberTwo);
 	}
 	
-	public Double Division() {
-		
+	@RequestMapping(value = "/division/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+	public Double Division(@PathVariable(value = "numberOne") String numberOne, @PathVariable(value = "numberTwo") String numberTwo) {
+		if (!IsNumeric(numberOne) || !IsNumeric(numberTwo)){
+			throw new UnsupportedMathOperationException("set a numeric value");
+		}
+		return convertToDouble(numberOne) / convertToDouble(numberTwo);
 	}
 	
-	public Double Avarege() {
+	@RequestMapping(value = "/avarege/{numbers}", method = RequestMethod.GET)
+	public Double Avarege(@PathVariable(value = "numbers") String numbers) {
 		
+		String[] numberArr = numbers.split(",");
+		Double sum = 0.0;
+		Integer count = 0;
+		
+		for (String number : numberArr) {
+			
+			if (!IsNumeric(number)){
+				throw new UnsupportedMathOperationException("set a numeric value");
+			}
+			sum += convertToDouble(number);
+			count++;
+		}
+		
+		return sum / count;
 	}
 	
-	public Double squareRoot() {
-		
+	@RequestMapping(value = "/square_root/{numberOne}", method = RequestMethod.GET)
+	public Double squareRoot(@PathVariable(value = "numberOne") String numberOne) {
+		if (!IsNumeric(numberOne)){
+			throw new UnsupportedMathOperationException("set a numeric value");
+		}
+		return Math.sqrt(convertToDouble(numberOne));
 	}
 
 	private Double convertToDouble(String strNumber) {
